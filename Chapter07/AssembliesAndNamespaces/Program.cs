63e677239abd6605192729b58f3201370cfe0382
apoda008 <161513572+apoda008@users.noreply.github.com>
2025-06-17 14:21:13 -0700

    .NET frameworks, packages and namespaces

---
+++ Chapter07/AssembliesAndNamespaces/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+using System.Xml.Linq; //To use Xdocument
+
+//NAMESPACES, PACKAGES, FRAMEWORKS (.NET)
+XDocument doc = new();
+
+string s1 = "Hello";
+String s2 = "World";
+WriteLine($"{s1} {s2}");
