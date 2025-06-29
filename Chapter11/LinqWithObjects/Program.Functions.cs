a8595432cc3ebad15bbeed37b96996003ab0dffa
apoda008 <161513572+apoda008@users.noreply.github.com>
2025-06-28 19:26:44 -0700

    Chapter 11: Filtering using Where, Simplifying by removing explicit delegate, Lambda and LINQ, Sorting: Single property, sub propertym and itself with LINQ. Var vs Type declaration for LINQ queries. Filtering by type and working with Sets/Bags

---
+++ Chapter11/LinqWithObjects/Program.Functions.cs
@@ -0,0 +1,133 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+partial class Program 
+{
+    private static void DeferredExecution(string[] names) 
+    {
+        SectionTitle("Deferred Execution");
+
+        //Question: Which names end with an M 
+        //(Using a LINQ extension method)
+        var query1 = names.Where(name => name.EndsWith("m"));
+
+        //Question: which names with an M
+        //(Using LINQ query comprehension syntax)
+        var query2 = from name in names where name.EndsWith("m") select name;
+
+        //Answer returned as an array of strings containing Pam and Jim
+        string[] result1 = query1.ToArray();
+
+        //answer returned as alist of strings contain Pam and Jim 
+        List<string> result2 = query2.ToList();
+
+        //Answer returned as we enumerate over the results 
+        foreach (string name in query1)
+        {
+            WriteLine(name); //outputs pam
+            names[2] = "Jimmy"; //Change jim to Jimmy 
+            //on the second iteration jimmy does not 
+            //end iwth an 'm' so it does not get output
+        }
+    }
+
+    private static void FilteringUsingWhere(string[] names) 
+    {
+        //var query = names.Where(new Func<string, bool>(NameLongerThanFour));
+
+        //The compiler creates the delegate automatically 
+        //var query = names.Where(NameLongerThanFour);
+
+        //using lambda expressions instead of a named method
+        
+        //var query = names.Where(names => names.Length > 4).OrderBy(name => name.Length).ThenBy(name => name);
+        // ^- ThenBy; when values are the same from the 'OrderBy' call it is 'then' sorted by the next
+        // expression, in this case alphabetical. 
+
+        //before NET 7: you would have to call the orderby meth and pass a lambda that selects the items themselves
+        //-- var query = names.OrderBy(name => name);
+
+        //After Net7: 
+        //-- var query names.Order()
+
+        IOrderedEnumerable<string> query = names
+            .Where(name => name.Length > 4)
+            .OrderBy(name => name.Length)
+            .ThenBy(name => name);
+
+        foreach (string item in query) 
+        {
+
+            WriteLine(item);
+        }
+    }
+
+    static bool NameLongerThanFour(string name) 
+    { 
+        //returns true for a name longer than four characters
+        return name.Length > 4;
+    }
+
+    static void FilteringByType() 
+    {
+        SectionTitle("Filtering by type");
+        List<Exception> exceptoins = new()
+        {
+            new ArgumentException(), new SystemException(),
+            new IndexOutOfRangeException(), new InvalidOperationException(),
+            new NullReferenceException(), new InvalidCastException(),
+            new OverflowException(), new DivideByZeroException(),
+            new ApplicationException()
+        };
+
+        IEnumerable<ArithmeticException> arithmeticExceptionsQuery =
+            exceptoins.OfType<ArithmeticException>();
+
+        foreach (ArithmeticException exception in arithmeticExceptionsQuery) 
+        {
+            WriteLine(exception);
+        }
+    }
+
+    static void Output(IEnumerable<string> cohort, string description = "") 
+    {
+        if (!string.IsNullOrEmpty(description)) 
+        { 
+            WriteLine(description);
+        }
+
+        Write(" ");
+        WriteLine(string.Join(", ", cohort.ToArray()));
+        WriteLine();
+    }
+
+    static void WorkingWithSet()
+    {
+        string[] cohort1 = { "Rachel", "Gareth", "Jonathan", "George" };
+
+        string[] cohort2 = { "Jack", "Stephen", "Daniel", "Jack", "Jared" };
+
+        string[] cohort3 = { "Declan", "Jack", "Jack", "Jasmine", "Conor" };
+
+        SectionTitle("The Cohorts");
+
+        Output(cohort1, "Chohort 1");
+        Output(cohort2, "Cohort 2");
+        Output(cohort3, "Cohort 3");
+
+        SectionTitle("Set Operatios");
+
+        Output(cohort2.Distinct(), "cohort2.Distinct()");
+        Output(cohort2.DistinctBy(name => name.Substring(0, 2)),
+            "cohort2.DistinctBy(name => name.Substring(0,2))");
+        Output(cohort2.Union(cohort3), "cohort2.Union(cohort3)");
+        Output(cohort2.Concat(cohort3), "cohort2.Concat(cohort3)");
+        Output(cohort2.Intersect(cohort3), "cohort2.Intersect(cohort3)");
+        Output(cohort2.Except(cohort3), "cohort2.Except(cohort3)");
+        Output(cohort1.Zip(cohort2, (c1, c2) => $"{c1} matched with {c2}"), "cohort1.zip(cohort2)");
+        /*
+         With zip if there an unequal numbers of items in the two sequences, then some items will not 
+        have a matching partner. Those without a partner, like Jared, will not be included in the result
+         */
+    }
+
+}
---
+++ Chapter11/LinqWithObjects/Program.cs
@@ -0,0 +1,69 @@
+﻿/*
+ Methods that start with AS, like AsEnumerable, cast the sequence into a diff type but do not allocate 
+memory so those medthods are fast. Methods that start with TO, like ToList allocate memory for a new 
+sequence of items so they can be slow and will always use more memory resources
+ */
+
+//A string array is a sequence that implements IEnumerable<string>
+string[] names = { "Micheal", "Pam", "Jim", "Deiwght", "Angela", "Kevin", "Toby", "Creed" };
+
+//DeferredExecution(names);
+
+//FILTERING ENTITIES USING WHERE
+/*
+    Where is an extension  method. It does not exist on teh array type. To make the Where extension 
+method availabl, we must import the system.linq namespace. This is implicity imported by defualt in the 
+new .NET 6 and later projects.
+ */
+
+
+
+//FilteringUsingWhere(names);
+
+//SIMPLIFYING THE CODE BY REMOVING THE EXPLICIT DELEGATE INSTANTIATION 
+//----See Program.Functions.cs
+
+//Targeting a lambda expression
+//----See Program.Functions.cs
+
+//LAMBDA EXPRESSION WITH DEFAULT PARAMETER VALUES 
+
+// var query = names.where((string name = "bob") => name.Length > 4);
+
+//SORTING AND MORE 
+// OrderBy and ThenBy for sorting 
+
+//SORTING BY A SINGLE PROPERTY USING OrderBy
+//---See var query in Program.Functions.cs
+/*
+ Extension methods can be chained if they previous method returns another sequence, that is, a type that implements the IEnumerable<T> interface
+ */
+
+//SORTING BY A SUBSEQUENT PROPERTY USING ThenBy
+//---See var query in Program.Functions.cs
+
+//SORTING BY THE ITEM ITSELF
+//---See var query in Program.Functions.cs
+
+//DECLARING A QUERY USING VAR OR A SPECIFIED TYPE
+//---See var query in Program.Functions.cs
+
+/*
+ With LINQ is it convenient to use var to declare the query object bcause the return type frequently changes as you work on a LINQ expression. For 
+example, this query started as an IEnumerable<string> and is currently an IOrderedEnumerable<string>
+ 
+ */
+
+//FILTERING BY TYPE
+//FilteringByType();
+
+//WORKING WITH SETS AND BAGS
+/*
+ Set: is a collection of one or more unique objects 
+
+Multiset, aka bag: is a collection of one or more objects that can have duplicates
+ */
+
+WorkingWithSet();
+
+//SO FAR THIS HAS BEEN WORK WITH IN MEMORY OBJECTS NOW IT WILL BE DATABASES FOLLOWING THIS
