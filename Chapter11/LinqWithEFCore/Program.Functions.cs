a63e915b853e29d1cead2a3b098f7e8ea6a057a0
apoda008 <161513572+apoda008@users.noreply.github.com>
2025-06-30 19:31:18 -0700

    Checking for empy sequences, paging with LINQ, LINQ query comprehension syntax
    Chapter 11 Completed

--- Chapter11/LinqWithEFCore/Program.Functions.cs
+++ Chapter11/LinqWithEFCore/Program.Functions.cs
@@ -1,6 +1,7 @@
 ﻿using Northwind.EntityModels; //to use northiwinddb, category, product
 using Microsoft.EntityFrameworkCore;
-using System.Security.Permissions; // to use dbset<t>
+using System.Security.Permissions;
+using System.Runtime.Intrinsics.Arm; // to use dbset<t>
 
 partial class Program 
 {
@@ -133,7 +134,133 @@ partial class Program
      Selector para are lambda expressions that select sub-elements for different purposes. 
     For example, ToLookUp has a keySelector to select the part of each item that will be 
     the value
-     
      */
+
+    private static void AggregateProducts()
+    {
+        SectionTitle("Aggregate Products");
+
+        using NorthwindDb db = new();
+
+        //Try to get an efficient count from EF Core DbSet<T>
+        if (db.Products.TryGetNonEnumeratedCount(out int countDbSet))
+        {
+            WriteLine($"{"Product count from Dbset:",-25} {countDbSet,10}");
+        }
+        else
+        {
+            WriteLine("Products DbSet does not have a count property");
+        }
+
+        //Try to get an efficient count from a list<t>
+        List<Product> products = db.Products.ToList();
+
+        if (products.TryGetNonEnumeratedCount(out int countList))
+        {
+            WriteLine($"{"Product count from list:",-25} {countList,10}");
+
+        }
+        else
+        {
+            WriteLine("Products list does not have a Count Property");
+
+        }
+
+        WriteLine($"{"Product count:",-25} {db.Products.Count(),10}");
+
+        WriteLine($"{"Discontinued product count:",27} {db.Products.Count(product => product.Discontinued),8}");
+
+        WriteLine($"{"Highest product price:", -25} {db.Products.Max(p => p.UnitPrice), 10:$#,##0.00}");
+
+        WriteLine($"{"Sum of units in stock:",-25} {db.Products.Sum(p => p.UnitsInStock),10:N0}");
+
+        WriteLine($"{"Sum of units on order:",-25} {db.Products.Sum(p => p.UnitsOnOrder),10:N0}");
+
+        WriteLine($"{"Average unit price:",-25} {db.Products.Average(p => p.UnitPrice),10:$#,##0.00}");
+
+        WriteLine($"{"Value of units in stock",-25} {db.Products.Sum(p => p.UnitPrice * p.UnitsInStock),10:$#,##0.00}");
+
+    }
+
+    /*
+     GOOD PRACTICE: Getting a count can seem like a simple operation but counting can be costly. A dbSet<> like Products 
+    does not have a count property so TryGetNonEnmueratedCount returns False. A List<T> like products does have a count 
+    property because it implements ICollection so TryGetNonEnmueratedCount returns true. (In this case we had to 
+    instantiate a list, which is itself a costly operations but if you already have a list and need to know the num of 
+    items, then this would be effecient) You can always call count() on DbSet<> but i can be slow because it might have to 
+    enumerate the sequence depending on the data provider implementation. For any araay, use the Length property to get a 
+    count. You can pass a lambda expression to Count() to filter which items in the sequence should be counted which you 
+    cannot do with either the Count or Length Properties
+     */
+
+    private static void OutputTableOfProducts(Product[] products, int currentPage, int totalPages) 
+    {
+        string line = new('-', count: 73);
+        string lineHalf = new('-', count: 30);
+
+        WriteLine(line);
+        WriteLine("{0,4} {1,-40} {2,12:C} {3,-15}",
+                "ID", "Product Name", "Unit Price", "Discontinued");
+        WriteLine(line);
+
+        foreach (Product p in products) 
+        {
+            WriteLine("{0,4} {1,-40} {2,12:C} {3,-15}",
+                p.ProductId, p.ProductName, p.UnitPrice, p.Discontinued);
+
+        }
+
+        WriteLine("{0} Page {1} of {2} {3}",
+            lineHalf, currentPage + 1, totalPages + 1, lineHalf);
+
+        /*
+         As usual in computing, our code will start counting from 0, so we need to add 1 to both the currentpage and totalpages 
+        \count before showing there values
+         */
+    }
+
+    private static void OutputPageOfProducts(IQueryable<Product> products, int pageSize, int currentPage, int totalPages)
+    {
+        //we must order data before skipping and taking to ensure
+        //the data is not randomly sorted in each page 
+        var pagingQuery = products.OrderBy(p => p.ProductId)
+            .Skip(currentPage * pageSize).Take(pageSize);
+
+        Clear(); // clear the console screen
+
+        SectionTitle(pagingQuery.ToQueryString());
+
+        OutputTableOfProducts(pagingQuery.ToArray(), currentPage, totalPages);  
+    
+    }
+
+    private static void PagingProducts() 
+    {
+        SectionTitle("Paging products");
+
+        using NorthwindDb db = new();
+
+        int pagesize = 10;
+        int currentpage = 0; 
+        int productcount = db.Products.Count();
+        int totalpages = productcount / pagesize;
+
+        while (true) //  use break to escape this infinite loop
+        { 
+            OutputPageOfProducts(db.Products, pagesize, currentpage, totalpages);
+
+            Write("Press <- to page back, press -> to page forward, any key to exit)");
+            ConsoleKey key = ReadKey().Key;
+
+            if (key == ConsoleKey.LeftArrow)
+                currentpage = currentpage == 0 ? totalpages : currentpage - 1;
+            else if (key == ConsoleKey.RightArrow)
+                currentpage = currentpage == totalpages ? 0 : currentpage + 1;
+            else
+                break;
+
+            WriteLine();
+        }
+    }
 
 }
--- Chapter11/LinqWithEFCore/Program.cs
+++ Chapter11/LinqWithEFCore/Program.cs
@@ -10,7 +10,62 @@
 //GROUP-JOINING SEQUENCES 
 //GroupJoinCategoriesAndProducts();
 
-//GROUPING FOR LOOKUPS
-ProductsLookUp();
+//GROUPING FOR LOOKUPS ======LAST COMMIT=======
+//ProductsLookUp();
 
 //AGGREGATING AND PAGING SEQUENCES
+//AggregateProducts();
+
+//CHECKING FOR AN EMPTY SEQUENCE
+/*
+ Call count() and see if it returns zero - worst way to do this 
+
+call LINQ Any() and see if it returns true - better than count() but still not best 
+
+Get the sequence's count prop (if it has one) and see if its greater than zero. 
+
+Get the sequences Length prop (if it has one) and see if its greater than zero,
+ */
+
+//BE CAREFUL WITH COUNT
+//The short of it: avoid calling it unless its a type that has it
+
+//PAGING WITH LINQ
+PagingProducts();
+
+/*
+ GOOD PRACTICE: You should always order data before calling Skip and Take if you want 
+to implement paging. This is because each time you execute a query, the linq provider 
+does not have to guarantee to return the data in the same order unless you have 
+specified it. Therefore, if the SQlite provider wanted to, the first time you request 
+a page of products they might be in productid order but the next time you request a 
+page of products they might be in unitprice oder or a random order and that would 
+confuse the user. in practice, at least for relational databases, the defualt order 
+is usually by its index on the primary key
+ */
+
+//SWEETENING LINQ SYNTAX WITH SYNTACTIC SUGAR
+//aka LINQ query comprehension syntax
+
+/*
+ 
+ OG without sugar 
+var query = names 
+    .Where(name => name.length > 4
+    .OrderBy(name => name.length
+    .ThenBy(name => name);
+
+With sugar:
+
+var query = from name in names 
+    where name.length > 4 
+    orderby name.length, name 
+    select name;
+ 
+ 
+ the select keyword is always required for LINQ query comprehension syntax. the select 
+extension method is optional when using extension methods and lambda expression because if 
+you do not call select, then the whole item is implicitly selected
+ 
+ 
+ */
